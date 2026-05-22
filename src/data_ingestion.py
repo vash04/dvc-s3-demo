e9316380712ed262a9d27a1273a310fd58912384
sarvesh <joshisarvesh04071996@gmail.com>
2026-05-22 14:22:43 +0530

    initial commit with pca on s3

---
+++ src/data_ingestion.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+import os
+
+df = pd.read_csv('./data/raw/student_placement_dataset_balanced.csv')
+
+# Separating features and target variable
+X = df.drop(columns=['placed'])
+y = df['placed']
+
+# Scaling the data
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+# Applying PCA
+pca = PCA(n_components=3)
+X_pca = pca.fit_transform(X_scaled)
+
+# Creating a DataFrame with PCA results
+df_pca = pd.DataFrame(data=X_pca, columns=['PC1', 'PC2', 'PC3'])
+df_pca['placed'] = y.values
+
+df_pca.to_csv(os.path.join('data','processed','student_performance_pca.csv'), index=False)
